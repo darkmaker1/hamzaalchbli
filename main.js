5bc5b480f41f33e9e52700126cb6b617edc73fb6
Hamza Alchbli <hamzaalshibli0@gmail.com>
2022-07-13 11:03:38 +0200

    javascript-fixed

--- main.js
+++ main.js
@@ -11,17 +11,19 @@ function reveal() {
         var revealertop = revealers[i].getBoundingClientRect().top;
         var revealerbottom = revealers[i].getBoundingClientRect().bottom;
         if (revealertop < windowheight - revealerpoint) {
-            revealers[i].classList.add('fade-in')
             revealers[i].classList.remove('fade-out');
-        } else {
             revealers[i].classList.add('fade-in');
-            revealers[i].classList.add('fade-out');
-
-        };
-        if (revealerbottom < 0 + revealerpoint) {
-            revealers[i].classList.remove('fade-in');
-            revealers[i].classList.add('fade-out');
+            // revealers[i].style.backgroundColor = "blue";
         }
+        //  else {
+        //     revealers[i].classList.add('fade-in');
+        //     revealers[i].classList.add('fade-out');
+
+        // };
+        // if (revealerbottom < 0 + revealerpoint) {
+        //     revealers[i].classList.remove('fade-in');
+        //     revealers[i].classList.add('fade-out');
+        // }
     } 
 };
 
