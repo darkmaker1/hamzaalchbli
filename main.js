467d2abfd5d6fd97cb2d2a1b9501db4bd7d849f7
Hamza Alchbli <hamzaalshibli0@gmail.com>
2022-07-20 22:05:55 +0200

    back to original

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // reveal point from bottom and top of the window
-var revealerpoint = 100;
+var revealerpoint = 150;
 window.addEventListener('scroll', reveal);
 reveal();
 
@@ -11,19 +11,17 @@ function reveal() {
         var revealertop = revealers[i].getBoundingClientRect().top;
         var revealerbottom = revealers[i].getBoundingClientRect().bottom;
         if (revealertop < windowheight - revealerpoint) {
+            revealers[i].classList.add('fade-in')
             revealers[i].classList.remove('fade-out');
-            revealers[i].classList.add('fade-in');
-            // revealers[i].style.backgroundColor = "blue";
-        }
-        //  else {
-        //     revealers[i].classList.add('fade-in');
-        //     revealers[i].classList.add('fade-out');
+        } else {
+            revealers[i].classList.remove('fade-in');
+            revealers[i].classList.add('fade-out');
 
-        // };
-        // if (revealerbottom < 0 + revealerpoint) {
-        //     revealers[i].classList.remove('fade-in');
-        //     revealers[i].classList.add('fade-out');
-        // }
+        };
+        if (revealerbottom < 0 + revealerpoint) {
+            revealers[i].classList.remove('fade-in');
+            revealers[i].classList.add('fade-out');
+        }
     } 
 };
 
